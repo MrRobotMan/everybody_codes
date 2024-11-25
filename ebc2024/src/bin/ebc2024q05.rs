64bcea3c22f7f0719a2b35862a34378de3d252e2
David Weiss <dweiss2@gmail.com>
2024-11-25 00:50:02 -0500

    2024 Quest 5 Part 3

--- ebc2024/src/bin/ebc2024q05.rs
+++ ebc2024/src/bin/ebc2024q05.rs
@@ -1,5 +1,5 @@
 use ebclib::read_lines;
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
     let input = read_lines("ebc2024/inputs/quest05.1.txt");
@@ -8,8 +8,8 @@ fn main() {
     let input = read_lines("ebc2024/inputs/quest05.2.txt");
     println!("Part 2: {}", part_two(input));
 
-    let _input = read_lines("ebc2024/inputs/quest05.3.txt");
-    println!("Part 3: {}", part_three());
+    let input = read_lines("ebc2024/inputs/quest05.3.txt");
+    println!("Part 3: {}", part_three(input));
 }
 
 fn part_one(input: Vec<String>) -> u64 {
@@ -22,11 +22,12 @@ fn part_one(input: Vec<String>) -> u64 {
 
 fn part_two(input: Vec<String>) -> u64 {
     let mut dance = Dance::new(&input);
-    dance.dance_forever(2024)
+    dance.dance_until(2024)
 }
 
-fn part_three() -> String {
-    "Unsolved".into()
+fn part_three(input: Vec<String>) -> u64 {
+    let mut dance = Dance::new(&input);
+    dance.dance_forever()
 }
 
 #[derive(Debug)]
@@ -47,28 +48,17 @@ impl Dance {
 
     fn step(&mut self, clapper_col: usize) -> u64 {
         let clapper = self.columns[clapper_col].pop_front().unwrap();
-        let mut mod_clapper = clapper as usize;
         let next_column = (clapper_col + 1) % self.columns.len();
-        let next_column_len = self.columns[next_column].len();
-        while mod_clapper > 0 {
-            if mod_clapper <= next_column_len {
-                self.columns[next_column].insert(mod_clapper - 1, clapper);
-                break;
-            } else if mod_clapper < next_column_len * 2 {
-                self.columns[next_column].insert(next_column_len * 2 - mod_clapper + 1, clapper);
-                break;
-            } else {
-                mod_clapper -= next_column_len * 2;
-            }
-        }
-        let mut res = 0;
-        for col in self.columns.iter() {
-            res = res * 10_u64.pow(col[0].ilog10() + 1) + col[0]
-        }
-        res
+        let next_column_len = 2 * self.columns[next_column].len();
+        let remainder = (clapper as usize - 1) % next_column_len;
+        let position = remainder.min(next_column_len - remainder);
+        self.columns[next_column].insert(position, clapper);
+        self.columns
+            .iter()
+            .fold(0, |acc, col| acc * 10_u64.pow(col[0].ilog10() + 1) + col[0])
     }
 
-    fn dance_forever(&mut self, target: u64) -> u64 {
+    fn dance_until(&mut self, target: u64) -> u64 {
         let mut count = HashMap::new();
         let mut round = 0;
         loop {
@@ -80,6 +70,22 @@ impl Dance {
             }
         }
     }
+
+    fn dance_forever(&mut self) -> u64 {
+        let mut states = HashSet::new();
+        let mut round = 0;
+        while states.insert(self.columns.clone()) {
+            self.step(round % self.columns.len());
+            round += 1;
+        }
+        states.iter().map(|v| calc(v)).max().unwrap()
+    }
+}
+
+fn calc(value: &[VecDeque<u64>]) -> u64 {
+    value
+        .iter()
+        .fold(0_u64, |acc, v| acc * 10_u64.pow(v[0].ilog10() + 1) + v[0])
 }
 
 #[cfg(test)]
@@ -98,6 +104,12 @@ mod tests {
     #[test]
     fn test_two() {
         let mut dance = Dance::new(&["2 3 4 5", "6 7 8 9"]);
-        assert_eq!(50877075, dance.dance_forever(2024));
+        assert_eq!(50877075, dance.dance_until(2024));
+    }
+
+    #[test]
+    fn test_three() {
+        let mut dance = Dance::new(&["2 3 4 5", "6 7 8 9"]);
+        assert_eq!(6584, dance.dance_forever());
     }
 }
