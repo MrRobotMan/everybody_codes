028495a8209c49875a6d67f95885fc1bdc0466f0
David Weiss <dweiss2@gmail.com>
2024-11-26 09:28:11 -0500

    2024 Quest 8 Part 1

--- ebc2024/src/bin/ebc2024q08.rs
+++ ebc2024/src/bin/ebc2024q08.rs
@@ -1,18 +1,24 @@
 use ebclib::read_lines;
-    
+
 fn main() {
-    let _input = read_lines("ebc2024/inputs/quest08.1.txt");
-    println!("Part 1: {}", part_one());
+    let input = read_lines("ebc2024/inputs/quest08.1.txt")[0]
+        .parse::<i64>()
+        .unwrap();
+    println!("Part 1: {}", part_one(input));
 
     let _input = read_lines("ebc2024/inputs/quest08.2.txt");
     println!("Part 2: {}", part_two());
 
     let _input = read_lines("ebc2024/inputs/quest08.3.txt");
     println!("Part 3: {}", part_three());
-    }
+}
 
-fn part_one() -> String {
-    "Unsolved".into()
+fn part_one(available: i64) -> i64 {
+    let mut next_biggest = 0_i64;
+    while next_biggest.pow(2) < available {
+        next_biggest += 1;
+    }
+    (next_biggest.pow(2) - available) * (2 * next_biggest - 1)
 }
 
 fn part_two() -> String {
@@ -29,9 +35,8 @@ mod tests {
 
     #[test]
     fn test_one() {
-        let expected = 1;
-        let actual = 0;
+        let expected = 21;
+        let actual = part_one(13);
         assert_eq!(expected, actual);
     }
 }
-    
