d503d326c1e8c9f87011059dae5454158e4aab1b
David Weiss <dweiss2@gmail.com>
2024-11-26 10:57:26 -0500

    2024 Quest 8 Part 3

--- ebc2024/src/bin/ebc2024q08.rs
+++ ebc2024/src/bin/ebc2024q08.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use ebclib::read_lines;
 
 fn main() {
@@ -11,8 +13,10 @@ fn main() {
         .unwrap();
     println!("Part 2: {}", part_two(priests, 1111, 20240000));
 
-    let _input = read_lines("ebc2024/inputs/quest08.3.txt");
-    println!("Part 3: {}", part_three());
+    let priests = read_lines("ebc2024/inputs/quest08.3.txt")[0]
+        .parse::<u64>()
+        .unwrap();
+    println!("Part 3: {}", part_three(priests, 10, 202400000));
 }
 
 fn part_one(available: i64) -> i64 {
@@ -39,8 +43,34 @@ fn part_two(priests: i64, acolytes: i64, blocks: i64) -> i64 {
     (total - blocks) * width
 }
 
-fn part_three() -> String {
-    "Unsolved".into()
+fn part_three(priests: u64, acolytes: u64, blocks: u64) -> u64 {
+    let mut heights = VecDeque::new();
+    let mut width = 1;
+    let mut thickness = 1;
+    heights.push_front(1);
+    #[cfg(test)]
+    println!("{heights:?}");
+    let mut total = 1;
+    loop {
+        // Assuming multiplications will become large, we're distributing the modulo.
+        // (a * b) % n == ((a % n) * (b % n)) %n
+        thickness = ((thickness % acolytes) * (priests % acolytes)) % acolytes + acolytes;
+        width += 2;
+        heights.iter_mut().for_each(|h| *h += thickness);
+        heights.push_front(thickness);
+        heights.push_back(thickness);
+        total += width * thickness;
+        let removed = heights
+            .iter()
+            .skip(1)
+            .take(heights.len() - 2)
+            .map(|&h| (priests % acolytes * width % acolytes * h % acolytes) % acolytes)
+            .collect::<Vec<_>>();
+        let removed = removed.iter().sum::<u64>();
+        if (total - removed) > blocks {
+            return total - removed - blocks;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -58,6 +88,13 @@ mod tests {
     fn test_two() {
         let expected = 27;
         let actual = part_two(3, 5, 50);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_three() {
+        let expected = 2;
+        let actual = part_three(2, 5, 160);
         assert_eq!(expected, actual);
     }
 }
