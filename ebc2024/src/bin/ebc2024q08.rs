72d4b134770d04225159801520d71d27de218d15
David Weiss <dweiss2@gmail.com>
2024-11-26 09:56:30 -0500

    2024 Quest 8 Part 2

--- ebc2024/src/bin/ebc2024q08.rs
+++ ebc2024/src/bin/ebc2024q08.rs
@@ -1,13 +1,15 @@
 use ebclib::read_lines;
 
 fn main() {
-    let input = read_lines("ebc2024/inputs/quest08.1.txt")[0]
+    let blocks = read_lines("ebc2024/inputs/quest08.1.txt")[0]
         .parse::<i64>()
         .unwrap();
-    println!("Part 1: {}", part_one(input));
+    println!("Part 1: {}", part_one(blocks));
 
-    let _input = read_lines("ebc2024/inputs/quest08.2.txt");
-    println!("Part 2: {}", part_two());
+    let priests = read_lines("ebc2024/inputs/quest08.2.txt")[0]
+        .parse::<i64>()
+        .unwrap();
+    println!("Part 2: {}", part_two(priests, 1111, 20240000));
 
     let _input = read_lines("ebc2024/inputs/quest08.3.txt");
     println!("Part 3: {}", part_three());
@@ -21,8 +23,20 @@ fn part_one(available: i64) -> i64 {
     (next_biggest.pow(2) - available) * (2 * next_biggest - 1)
 }
 
-fn part_two() -> String {
-    "Unsolved".into()
+fn part_two(priests: i64, acolytes: i64, blocks: i64) -> i64 {
+    let mut width = 1;
+    let mut thickness = 1;
+    let mut total = 1;
+    while total < blocks {
+        // Assuming multiplications will become large, we're distributing the modulo.
+        // (a * b) % n == ((a % n) * (b % n)) %n
+        thickness = ((thickness % acolytes) * (priests % acolytes)) % acolytes;
+        width += 2;
+        total += width * thickness;
+        #[cfg(test)]
+        println!("Adding {thickness} * {width}. Total needed {total}");
+    }
+    (total - blocks) * width
 }
 
 fn part_three() -> String {
@@ -37,6 +51,13 @@ mod tests {
     fn test_one() {
         let expected = 21;
         let actual = part_one(13);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_two() {
+        let expected = 27;
+        let actual = part_two(3, 5, 50);
         assert_eq!(expected, actual);
     }
 }
