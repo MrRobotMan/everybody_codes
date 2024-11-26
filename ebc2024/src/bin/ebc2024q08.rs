7d42091d8eeb555083a77871002753827a9c068a
David Weiss <dweiss2@gmail.com>
2024-11-26 09:13:19 -0500

    2024 Quest 8 Started

---
+++ ebc2024/src/bin/ebc2024q08.rs
@@ -0,0 +1,37 @@
+use ebclib::read_lines;
+    
+fn main() {
+    let _input = read_lines("ebc2024/inputs/quest08.1.txt");
+    println!("Part 1: {}", part_one());
+
+    let _input = read_lines("ebc2024/inputs/quest08.2.txt");
+    println!("Part 2: {}", part_two());
+
+    let _input = read_lines("ebc2024/inputs/quest08.3.txt");
+    println!("Part 3: {}", part_three());
+    }
+
+fn part_one() -> String {
+    "Unsolved".into()
+}
+
+fn part_two() -> String {
+    "Unsolved".into()
+}
+
+fn part_three() -> String {
+    "Unsolved".into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+        let expected = 1;
+        let actual = 0;
+        assert_eq!(expected, actual);
+    }
+}
+    
