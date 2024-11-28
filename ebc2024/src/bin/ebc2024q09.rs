7b2a1797ff14cfcd95bed113e68d2c14338113d9
David Weiss <dweiss2@gmail.com>
2024-11-28 13:39:30 -0500

    2024 Quest 9 Part 2. Refactored to use dynamic programming

--- ebc2024/src/bin/ebc2024q09.rs
+++ ebc2024/src/bin/ebc2024q09.rs
@@ -1,40 +1,66 @@
 use ebclib::{read_lines, read_numbers};
 
-const STAMPS: [u64; 4] = [1, 3, 5, 10];
-
 fn main() {
     let input = read_numbers("ebc2024/inputs/quest09.1.txt");
     println!("Part 1: {}", part_one(&input));
 
-    let _input = read_lines("ebc2024/inputs/quest09.2.txt");
-    println!("Part 2: {}", part_two());
+    let input = read_numbers("ebc2024/inputs/quest09.2.txt");
+    println!("Part 2: {}", part_two(&input));
 
     let _input = read_lines("ebc2024/inputs/quest09.3.txt");
     println!("Part 3: {}", part_three());
 }
 
 fn part_one(sparkballs: &[u64]) -> u64 {
-    sparkballs.iter().map(|s| get_beetle_count(*s)).sum()
+    let stamps = [1, 3, 5, 10];
+    get_beetle_count(sparkballs, &stamps).iter().sum()
+    // sparkballs
+    //     .iter()
+    //     .map(|s| get_beetle_count(*s, &stamps))
+    //     .sum()
 }
 
-fn part_two() -> String {
-    "Unsolved".into()
+fn part_two(sparkballs: &[u64]) -> u64 {
+    let stamps = [1, 3, 5, 10, 15, 16, 20, 24, 25, 30];
+    get_beetle_count(sparkballs, &stamps).iter().sum()
+    // sparkballs
+    //     .iter()
+    //     .map(|s| get_beetle_count(*s, &stamps))
+    //     .sum()
 }
 
 fn part_three() -> String {
     "Unsolved".into()
 }
 
-fn get_beetle_count(mut sparkball: u64) -> u64 {
-    let mut total = 0;
-    for stamp in STAMPS.iter().rev() {
-        if *stamp <= sparkball {
-            let count = sparkball / stamp;
-            total += count;
-            sparkball -= count * stamp;
+fn get_beetle_count(sparkballs: &[u64], stamps: &[u64]) -> Vec<u64> {
+    let mut counts = vec![vec![0; *sparkballs.iter().max().unwrap() as usize + 1]; stamps.len()];
+    for sparkball in sparkballs {
+        if counts[0][*sparkball as usize] > 0 {
+            continue;
         }
+        stamps
+            .iter()
+            .rev()
+            .enumerate()
+            .rev()
+            .for_each(|(stamp_idx, stamp)| {
+                (1..=*sparkball).for_each(|index| {
+                    counts[stamp_idx][index as usize] = u64::MAX;
+                    let mut keep = u64::MAX;
+                    let mut drop = u64::MAX;
+                    if let Some(v) = index.checked_sub(*stamp) {
+                        keep = counts[stamp_idx][v as usize];
+                        keep = keep.saturating_add(1);
+                    }
+                    if stamp_idx < (stamps.len() - 1) {
+                        drop = counts[stamp_idx + 1][index as usize];
+                    }
+                    counts[stamp_idx][index as usize] = keep.min(drop);
+                })
+            });
     }
-    total
+    sparkballs.iter().map(|s| counts[0][*s as usize]).collect()
 }
 
 #[cfg(test)]
@@ -42,13 +68,22 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_simple_count() {
+        assert_eq!(
+            3,
+            get_beetle_count(&[19], &[1, 5, 6, 9]).iter().sum::<u64>()
+        );
+    }
+
+    #[test]
     fn test_one() {
         let sparkballs = [2, 4, 7, 16];
-        let counts = sparkballs
-            .iter()
-            .map(|s| get_beetle_count(*s))
-            .collect::<Vec<_>>();
-        assert_eq!([2, 2, 3, 3], counts.as_slice());
-        assert_eq!(10, counts.iter().sum::<u64>());
+        assert_eq!(10, part_one(&sparkballs));
+    }
+
+    #[test]
+    fn test_two() {
+        let sparkballs = [33, 41, 55, 99];
+        assert_eq!(10, part_two(&sparkballs));
     }
 }
