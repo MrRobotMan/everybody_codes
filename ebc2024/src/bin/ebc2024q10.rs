8959f51ec5122a14ec81f68ed7247444e0ce8588
David Weiss <dweiss2@gmail.com>
2024-12-01 12:02:47 -0600

    2024 Quest 10 Part 2

--- ebc2024/src/bin/ebc2024q10.rs
+++ ebc2024/src/bin/ebc2024q10.rs
@@ -6,14 +6,48 @@ fn main() {
     let grid = read_grid("ebc2024/inputs/quest10.1.txt");
     println!("Part 1: {}", part_one(grid));
 
-    let _grid = read_grid("ebc2024/inputs/quest10.2.txt");
-    println!("Part 2: {}", part_two());
+    let grids = read_grid("ebc2024/inputs/quest10.2.txt");
+    println!("Part 2: {}", part_two(grids));
 
     let _grid = read_grid("ebc2024/inputs/quest10.3.txt");
     println!("Part 3: {}", part_three());
 }
 
-fn part_one(grid: Vec<Vec<char>>) -> String {
+fn part_one(grid: Grid<char>) -> String {
+    find_word(&grid)
+}
+
+type Grid<T> = Vec<Vec<T>>;
+
+fn part_two(grids: Grid<char>) -> usize {
+    let mut shrines: Vec<Grid<char>> =
+        vec![vec![]; (grids.len() / 9 + 1) * (grids[0].len() / 9 + 1)];
+    let cols = grids[0].len() / 9 + 1;
+    for (idx, row) in grids.iter().enumerate() {
+        for (offset, ch) in row.chunks(9).enumerate() {
+            shrines[(idx / 9) * cols + offset].push(
+                ch.iter()
+                    .filter(|c| **c != ' ')
+                    .copied()
+                    .collect::<Vec<char>>(),
+            );
+        }
+    }
+    shrines.iter().map(|s| power(find_word(s))).sum::<usize>()
+}
+
+fn part_three() -> String {
+    "Unsolved".into()
+}
+
+fn power(word: String) -> usize {
+    word.chars()
+        .enumerate()
+        .map(|(idx, ch)| (idx + 1) * (ch as u8 - b'A' + 1) as usize)
+        .sum::<usize>()
+}
+
+fn find_word(grid: &Grid<char>) -> String {
     let ends = [0, 1, 6, 7];
     (2..6)
         .flat_map(|row| {
@@ -31,21 +65,13 @@ fn part_one(grid: Vec<Vec<char>>) -> String {
         .collect::<String>()
 }
 
-fn part_two() -> String {
-    "Unsolved".into()
-}
-
-fn part_three() -> String {
-    "Unsolved".into()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_one() {
-        let grid = read_grid(
+        let shrine = read_grid(
             "**PCBS**
 **RLNW**
 BV....PT
@@ -56,7 +82,8 @@ SG....MN
 **GMJH**",
         );
         let expected = "PTBVRCZHFLJWGMNS";
-        let actual = part_one(grid);
+        let actual = part_one(shrine);
         assert_eq!(expected, actual);
+        assert_eq!(1851, power(actual))
     }
 }
