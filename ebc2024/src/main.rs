fd458ea66a872d9b70a7991c200c2209126769d6
David Weiss <dweiss2@gmail.com>
2025-09-15 22:48:51 -0500

    Updated edition from 2021 to 2024

--- ebc2024/src/main.rs
+++ ebc2024/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello World!");
+    let mut res = vec![];
+    for day in 1..=12 {
+        if let Ok(out) = std::process::Command::new("cargo")
+            .args(["run", "--bin", &format!("ebc2024q{day:02}")])
+            .output()
+        {
+            res.push(format!("2024 Quest {day:02}").as_bytes().to_vec());
+            res.push(out.stdout);
+        } else {
+            res.push(
+                format!("Failed to run 2024 quest {day:02}")
+                    .as_bytes()
+                    .to_vec(),
+            );
+        };
+    }
+    for day in res.iter() {
+        println!("{}", str::from_utf8(day).unwrap());
+    }
 }
