45553f9822f966e568e3fd2b2fc3bedfe3d5c36e
David Weiss <ebdbbb@proton.me>
2025-12-22 23:30:04 -0600

    2024 Quest 17 Part 3

--- ebc2024/src/bin/ebc2024q17.rs
+++ ebc2024/src/bin/ebc2024q17.rs
@@ -6,20 +6,21 @@ use std::{
 use puzlib::{Vec2D, read_grid_to_map};
 
 fn main() {
-    let input = read_grid_to_map("ebc2024/inputs/quest17.1.txt")
-        .iter()
-        .filter_map(|(p, ch)| if *ch == '*' { Some(p.into()) } else { None })
-        .collect();
+    let input = lines_to_stars(read_grid_to_map("ebc2024/inputs/quest17.1.txt"));
     println!("Part 1: {}", part_one(input));
 
-    let input = read_grid_to_map("ebc2024/inputs/quest17.2.txt")
-        .iter()
-        .filter_map(|(p, ch)| if *ch == '*' { Some(p.into()) } else { None })
-        .collect();
+    let input = lines_to_stars(read_grid_to_map("ebc2024/inputs/quest17.2.txt"));
     println!("Part 2: {}", part_two(input));
 
-    let _input = read_grid_to_map("ebc2024/inputs/quest17.3.txt");
-    println!("Part 3: {}", part_three());
+    let input = lines_to_stars(read_grid_to_map("ebc2024/inputs/quest17.3.txt"));
+    println!("Part 3: {}", part_three(input));
+}
+
+fn lines_to_stars(input: Vec<((usize, usize), char)>) -> HashSet<Vec2D<usize>> {
+    input
+        .iter()
+        .filter_map(|(p, ch)| if *ch == '*' { Some(p.into()) } else { None })
+        .collect()
 }
 
 fn part_one(stars: HashSet<Vec2D<usize>>) -> usize {
@@ -30,8 +31,21 @@ fn part_two(stars: HashSet<Vec2D<usize>>) -> usize {
     best_constellation(stars)
 }
 
-fn part_three() -> String {
-    "Unsolved".into()
+fn part_three(mut stars: HashSet<Vec2D<usize>>) -> usize {
+    let mut constellations = Vec::new();
+    while !stars.is_empty() {
+        let cons = find_brilliant_constellation(&stars, 6);
+        for star in cons.iter() {
+            stars.remove(star);
+        }
+        constellations.push(best_constellation(cons));
+    }
+    constellations.sort_by(|a, b| b.cmp(a));
+    constellations
+        .into_iter()
+        .take(3)
+        .reduce(|acc, v| acc * v)
+        .unwrap()
 }
 
 fn best_constellation(stars: HashSet<Vec2D<usize>>) -> usize {
@@ -57,6 +71,30 @@ fn best_constellation(stars: HashSet<Vec2D<usize>>) -> usize {
     stars.len() + total_distance
 }
 
+fn find_brilliant_constellation(
+    stars: &HashSet<Vec2D<usize>>,
+    max_dist: usize,
+) -> HashSet<Vec2D<usize>> {
+    // Prim's Mimimum Spanning Tree
+    let mut constellation = HashSet::new();
+    let mut to_visit = BinaryHeap::new();
+    let start = *stars.iter().min().unwrap();
+    to_visit.push(Reverse((0, start)));
+    while let Some(Reverse((_, star))) = to_visit.pop() {
+        if constellation.contains(&star) {
+            continue;
+        }
+        constellation.insert(star);
+        for other in stars.iter() {
+            let dist = star.manhattan(*other);
+            if !constellation.contains(other) && dist < max_dist {
+                to_visit.push(Reverse((dist, *other)));
+            }
+        }
+    }
+    constellation
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -72,6 +110,21 @@ mod tests {
             (3_usize, 1).into(),
         ]);
         let actual = part_one(stars);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_two() {
+        let expected = 15624;
+        let actual = part_three(lines_to_stars(read_grid_to_map(
+            ".......................................
+..*.......*...*.....*...*......**.**...
+....*.................*.......*..*..*..
+..*.........*.......*...*.....*.....*..
+......................*........*...*...
+..*.*.....*...*.....*...*........*.....
+.......................................",
+        )));
         assert_eq!(expected, actual);
     }
 }
