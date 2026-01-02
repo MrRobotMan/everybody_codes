a316d8267efa412b923d25aaf5ec236d86908031
David Weiss <ebdbbb@proton.me>
2026-01-02 10:03:17 -0600

    Added story/event triple to args

--- ebclib/src/bin/build_quest.rs
+++ ebclib/src/bin/build_quest.rs
@@ -9,29 +9,29 @@ use std::{
 use toml_edit::{ArrayOfTables, DocumentMut, Item, Table, value};
 
 fn main() {
-    let Some((year, quest)) = get_args() else {
+    let Some((year, quest, triple)) = get_args() else {
         {
             println!("Invalid arguments. Please supply year and quest.");
             return;
         }
     };
 
-    match update_cargo(year, quest) {
+    match update_cargo(year, quest, &triple) {
         Ok(()) => println!("Updated {year} cargo."),
         Err(e) => println!("{e}"),
     };
-    match create_quest(year, quest) {
+    match create_quest(year, quest, &triple) {
         Ok(s) => println!("{s}"),
         Err(e) => println!("{e}"),
     }
-    match update_bacon(year, quest) {
+    match update_bacon(year, quest, &triple) {
         Ok(_) => println!("Bacon updated."),
         Err(e) => println!("{e}"),
     }
 }
 
-fn create_quest(year: i32, quest: u32) -> io::Result<String> {
-    let bin = PathBuf::from(format!("ebc{year}/src/bin/ebc{year}q{quest:02}.rs"));
+fn create_quest(year: i32, quest: u32, triple: &str) -> io::Result<String> {
+    let bin = PathBuf::from(format!("{triple}{year}/src/bin/ebc{year}q{quest:02}.rs"));
     if bin.exists() {
         return Ok(format!("{year} quest {quest} already exists. Skipping."));
     }
@@ -44,13 +44,13 @@ fn create_quest(year: i32, quest: u32) -> io::Result<String> {
         r#"use puzlib::read_lines;
     
 fn main() {{
-    let _input = puzlib::FileReader::new("ebc{year}/inputs/everbody_codes_e{year}_q{quest:02}_p1.txt");
+    let _input = puzlib::FileReader::new("{triple}{year}/inputs/everbody_codes_e{year}_q{quest:02}_p1.txt");
     println!("Part 1: {{}}", part_one());
 
-    let _input = puzlib::FileReader::new("ebc{year}/inputs/everbody_codes_e{year}_q{quest:02}_p2.txt");
+    let _input = puzlib::FileReader::new("{triple}{year}/inputs/everbody_codes_e{year}_q{quest:02}_p2.txt");
     println!("Part 2: {{}}", part_two());
 
-    let _input = puzlib::FileReader::new("ebc{year}/inputs/everbody_codes_e{year}_q{quest:02}_p3.txt");
+    let _input = puzlib::FileReader::new("{triple}{year}/inputs/everbody_codes_e{year}_q{quest:02}_p3.txt");
     println!("Part 3: {{}}", part_three());
     }}
 
@@ -84,11 +84,11 @@ mod tests {{
 }
 
 /// Update the year's cargo file for the new binary.
-fn update_cargo(year: i32, quest: u32) -> Result<(), Box<dyn Error>> {
-    let cargo_file = format!("ebc{year}/Cargo.toml");
+fn update_cargo(year: i32, quest: u32, triple: &str) -> Result<(), Box<dyn Error>> {
+    let cargo_file = format!("{triple}{year}/Cargo.toml");
     let mut cargo = get_existing_file(&cargo_file)?.parse::<DocumentMut>()?;
     let mut new_table = Table::new();
-    new_table["name"] = value(format!("ebc{year}q{quest:02}"));
+    new_table["name"] = value(format!("{triple}{year}q{quest:02}"));
     let bin = cargo
         .entry("bin")
         .or_insert(Item::ArrayOfTables(ArrayOfTables::new()))
@@ -102,17 +102,17 @@ fn update_cargo(year: i32, quest: u32) -> Result<(), Box<dyn Error>> {
 }
 
 /// Update bacon to call the binary when using run.
-fn update_bacon(year: i32, quest: u32) -> io::Result<()> {
+fn update_bacon(year: i32, quest: u32, triple: &str) -> io::Result<()> {
     let mut bacon = get_existing_file("bacon.toml")?
         .split('\n')
         .map(|s| s.to_string())
         .collect::<Vec<_>>();
     for line in bacon.iter_mut() {
         if line.contains("--package") {
-            *line = format!(r#"    "--package", "ebc{year}","#);
+            *line = format!(r#"    "--package", "{triple}{year}","#);
         }
         if line.contains("--bin") {
-            *line = format!(r#"    "--bin", "ebc{year}q{quest:02}","#);
+            *line = format!(r#"    "--bin", "{triple}{year}q{quest:02}","#);
         }
     }
     fs::write("bacon.toml", bacon.join("\n"))
@@ -128,11 +128,13 @@ fn get_existing_file(cargo_file: &str) -> io::Result<String> {
 }
 
 /// Gather the commandline arguments.
-fn get_args() -> Option<(i32, u32)> {
+fn get_args() -> Option<(i32, u32, String)> {
     let mut args = env::args();
+    let triple = args.next().expect("Need a year triple");
     match args.len() {
         1 => {
             // Rework this. Not sure when the event will start each year.
+            // Runs first Monday of November for 20 weekdays.
             let east_coast = FixedOffset::west_opt(5 * 60 * 60).unwrap();
             let today = Utc::now().with_timezone(&east_coast);
             if today
@@ -140,15 +142,15 @@ fn get_args() -> Option<(i32, u32)> {
                     .with_ymd_and_hms(today.year(), 12, 1, 0, 0, 0)
                     .unwrap()
             {
-                Some((today.year(), 1))
+                Some((today.year(), 1, triple))
             } else {
-                Some((today.year(), today.day()))
+                Some((today.year(), today.day(), triple))
             }
         }
         2 => {
             args.next();
             let year = args.next();
-            Some((year.unwrap().parse().unwrap(), 1))
+            Some((year.unwrap().parse().unwrap(), 1, triple))
         }
         3 => {
             args.next();
@@ -157,6 +159,7 @@ fn get_args() -> Option<(i32, u32)> {
             Some((
                 year.unwrap().parse().unwrap(),
                 quest.unwrap().parse().unwrap(),
+                triple,
             ))
         }
         _ => None,
